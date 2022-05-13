33241983a8efa150b17ca5f093c718883f65dd92
최웅석 <cus4703@gmail.com>
2022-05-13 23:11:21 +0900

    feat: retry 간단하게 해보기

--- src/main/java/cus/study/spring/sms/application/MessageService.java
+++ src/main/java/cus/study/spring/sms/application/MessageService.java
@@ -2,6 +2,8 @@ package cus.study.spring.sms.application;
 
 import cus.study.spring.sms.domain.Message;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
@@ -9,9 +11,20 @@ import java.util.Random;
 @Slf4j
 @Service
 public class MessageService {
+    private static int count = 0;
     private final Random random = new Random();
 
+    @Retryable(
+            value = RuntimeException.class,
+            maxAttempts = 2,
+            backoff = @Backoff(delay = 2000)
+    )
     public void sender(Message message) {
+        if (count < 2) {
+            count++;
+            throw new RuntimeException();
+        }
         log.info(message.senderTemplate() + message.getMessage());
+        count = 0;
     }
 }
