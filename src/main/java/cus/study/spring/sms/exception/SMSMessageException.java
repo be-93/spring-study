b9eed67853ef3139672d9af50087ec5dcbad747a
최웅석 <cus4703@gmail.com>
2022-05-17 20:19:21 +0900

    feat: message constructor add

--- src/main/java/cus/study/spring/sms/exception/SMSMessageException.java
+++ src/main/java/cus/study/spring/sms/exception/SMSMessageException.java
@@ -1,4 +1,11 @@
 package cus.study.spring.sms.exception;
 
 public class SMSMessageException extends RuntimeException {
+    public SMSMessageException() {
+        super();
+    }
+
+    public SMSMessageException(String message) {
+        super(message);
+    }
 }
